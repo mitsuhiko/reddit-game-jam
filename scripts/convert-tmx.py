9fbdec60a50559e429f5483c39497bdc198c8e5f
Armin Ronacher <armin.ronacher@active-4.com>
2011-03-26 01:49:25 +0100

    Added simple tmx convert script

---
+++ scripts/convert-tmx.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+converts tmx maps into our simplistic map format
+"""
+import sys
+from xml.etree import ElementTree as et
+
+
+class Map(object):
+
+    def __init__(self, width, height, tile_width, tile_height, tileset):
+        self.width = width
+        self.height = height
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.tileset = tileset
+        self.layers = []
+
+
+def relative_resource_path(path):
+    marker = '/resources/'
+    pos = path.find(marker)
+    if pos >= 0:
+        return path[pos + len(marker):]
+    return path
+
+
+def load_layer(data):
+    result = []
+    for i in xrange(0, len(data), 4):
+        gid = ord(data[i]) | ord(data[i + 1]) << 8 | \
+              ord(data[i + 2]) << 16 | ord(data[i + 3]) << 24
+        result.append(gid)
+    return result
+
+
+def read_tmx_file(filename):
+    with open(filename) as f:
+        doc = et.parse(f)
+    root = doc.getroot()
+    tileset = root.find('tileset')
+
+    map = Map(int(root.attrib['width']),
+              int(root.attrib['height']),
+              int(root.attrib['tilewidth']),
+              int(root.attrib['tileheight']),
+              relative_resource_path(tileset.getchildren()[0].attrib['source']))
+
+    for layer in root.findall('layer'):
+        data = layer.find('data')
+        assert data.attrib['encoding'] == 'base64'
+        val = data.text.decode('base64')
+        if data.attrib.get('compression'):
+            val = val.decode(data.attrib['compression'])
+        map.layers.append(load_layer(val))
+    return map
+
+
+def write_simple_format(m, filename):
+    with open(filename, 'wb') as f:
+        f.write(m.tileset + '\0')
+        f.write('%d\0%d\0%d\0%d\0' % (m.width, m.height, m.tile_width, m.tile_height))
+        for layer in m.layers:
+            f.write(''.join(map(chr, layer)))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print >> sys.stderr, 'usage: convert-tmx.py [input] [output]'
+        sys.exit(1)
+    input_file, output_file = sys.argv[1:]
+
+    m = read_tmx_file(input_file)
+    write_simple_format(m, output_file)
+
+
+if __name__ == '__main__':
+    main()
