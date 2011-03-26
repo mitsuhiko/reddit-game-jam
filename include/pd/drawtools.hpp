6c691e7f267b2067eaaf6edc75bf4cfef815de50
Armin Ronacher <armin.ronacher@active-4.com>
2011-03-26 20:04:05 +0100

    Added more drawing helpers (opengl wrappers)

--- include/pd/drawtools.hpp
+++ include/pd/drawtools.hpp
@@ -14,6 +14,25 @@ namespace pd {
     void draw_textured_quad(float x, float y, float width, float height, const pd::texture *texture);
     void draw_textured_quad(float x, float y, const pd::texture *texture);
     void draw_text(const std::string &text, float x, float y, const pd::font *font);
+
+    /* alternative names for opengl methods and some other thin
+       wrappers around them.  Why? Consistency and a chance to
+       move to custom matrix stacks. */
+    inline void push_matrix() { glPushMatrix(); }
+    inline void pop_matrix() { glPopMatrix(); }
+    inline void translate(float x, float y) { glTranslatef(x, y, 0.0f); }
+
+    inline void rotate(float angle)
+    {
+        glRotatef(angle, 0.0f, 0.0f, 1.0f);
+    }
+
+    inline void rotate_around_point(float angle, float x, float y)
+    {
+        translate(x, y);
+        rotate(angle);
+        translate(-x, -y);
+    }
 }
 
 #endif
