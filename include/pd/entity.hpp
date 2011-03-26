ce4393d868aacf7f18a7d325f241161b9ed1c867
Metanoia <ethedona@gmail.com>
2011-03-26 18:46:00 +0100

    More and more Box2D integration.

--- include/pd/entity.hpp
+++ include/pd/entity.hpp
@@ -3,19 +3,31 @@
 #include <pd/pd.hpp>
 #include <Box2D/Box2D.h>
 
+// Pixels to meters translation.
+#define ptm(a) (a / 30)
+#define mtp(a) (a * 30)
+
 namespace pd {
 
     class texture;
 
     class entity {
     public:
-        entity(b2World *world, float x = 0.0f, float y = 0.0f, float sizex = 0.0f, float sizey = 0.0f, float weight = 0.0f, float friction = 0.0f);
+        entity(b2World *world, float x = 0.0f, float y = 0.0f, float width = 0.0f, float height = 0.0f, float density = 0.0f, float friction = 0.0f);
         virtual ~entity() {}
 
         float x() const { return m_x; }
         void x(float val) { m_x = val; }
         float y() const { return m_y; }
         void y(float val) { m_y = val; }
+        float width() const { return m_width; }
+        void width(float val) { m_width = val; }
+        float height() const { return m_height; }
+        void height(float val) { m_height = val; }
+        float density() const { return m_density; }
+        void density(float val) { m_density = val; }
+        float friction() const { return m_friction; }
+        void friction(float val) { m_friction = val; }
         void move(float x, float y);
         void build_box2d_object();
         /* Syncs box2d body position with entity position. Should be called each frame. */
@@ -29,6 +41,10 @@ namespace pd {
         b2Body *m_body;
         float m_x;
         float m_y;
+        float m_width;
+        float m_height;
+        float m_density;
+        float m_friction;
     };
 }
 
--- src/player.cpp
+++ src/player.cpp
@@ -8,6 +8,12 @@ pd::player::player(b2World *world, float x, float y)
       m_walk_anim(pd::get_resource<pd::texture>("textures/test-pirate.png"), 9, 0.1f)
 {
     m_stance = kinetic_stance;
+
+    width(30.0f);
+    height(50.0f);
+    density(20.0f);
+    friction(1.02f);
+    build_box2d_object();
 }
 
 void pd::player::update(float dt)
