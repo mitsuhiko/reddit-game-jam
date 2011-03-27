1283ece26a7c36e471a47aae5ac5a1015e9b23ae
Armin Ronacher <armin.ronacher@active-4.com>
2011-03-27 17:30:36 +0200

    Added dummy kinetic enemy

---
+++ include/pd/kinetic_enemy.hpp
@@ -0,0 +1,24 @@
+#ifndef _INC_PD_KINETIC_ENEMY_HPP_
+#define _INC_PD_KINETIC_ENEMY_HPP_
+#include <pd/pd.hpp>
+#include <pd/enemy.hpp>
+#include <pd/animation.hpp>
+
+namespace pd {
+
+    class session;
+
+    class kinetic_enemy : public pd::enemy {
+    public:
+        kinetic_enemy(pd::game_session *session, float x = 0.0f, float y = 0.0f);
+
+        void update(float dt);
+        void local_render(float dt) const;
+
+    private:
+        stance_type m_stance;
+        pd::animation m_walk_anim;
+    };
+}
+
+#endif
